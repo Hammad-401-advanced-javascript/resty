9789ea5d1abe93fcb62fca7774ff5d7b889ca360
Hammadsoloman <hammadali95@yahoo.com>
2020-06-29 01:22:47 +0300

    done

---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './header.scss';
+
+const Header = () => {
+    return (
+        <header className='rHeader'>
+            <h1 className='REST'>RESTy</h1>
+        </header>
+    )
+
+}
+
+
+export default Header;
